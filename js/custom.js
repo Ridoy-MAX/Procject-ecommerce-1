65657505ccabd81aff41b971cac8275cf1e09738
Ridoy-MAX <pou4w98y@gmail.com>
2021-10-18 06:22:39 +0600

    testimonial & our blog done

--- js/custom.js
+++ js/custom.js
@@ -75,6 +75,50 @@ $('.new-product-banner').slick({
 
   });
  //new procdut slider ENd;
+
+
+ //new testi slider start;
+ $('.testi-banner').slick({
+  autoplay: true,
+  autoplaySpeed: 1800, 
+  dots: false,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  arrows:true,
+  prevArrow:'<i class="fas fa-chevron-left  previous-arrow"></i>',
+  nextArrow:'<i class="fas fa-chevron-right  next-arrow"></i>',
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+    
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+   
+      }
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+
+  });
+
+ //new testi slider end;
+
+
+
          /* Periodic Timer. Period is equal 10 days */
          $('#periodic-timer_period_days').syotimer({
           date: new Date(2050, 18, 19, 20, 30),
