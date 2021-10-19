c7e841499f53f09ffed6e480ac973b0c9fb2dc61
Ridoy-MAX <pou4w98y@gmail.com>
2021-10-20 03:09:01 +0600

    our desinger add

--- js/custom.js
+++ js/custom.js
@@ -115,7 +115,7 @@ $('.new-product-banner').slick({
 
   });
 
- //new testi slider end;
+    //new testi slider end;
 
 
 
@@ -128,5 +128,53 @@ $('.new-product-banner').slick({
           periodUnit: "d",
         });
           /* Periodic Timer. Period is equal 10 days */
+
+  
+     //about-counter;
+  
+      $('.counter').counterUp({
+        delay: 10,
+        time: 2000
+      });
+
+       //about patner slider START;
+       $('.patner-slider').slick({
+        autoplay: true,
+        autoplaySpeed: 1800, 
+        dots: false,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows:true,
+        prevArrow:'<i class="fas fa-chevron-left  previous-arrow"></i>',
+        nextArrow:'<i class="fas fa-chevron-right  next-arrow"></i>',
+        responsive: [
+          {
+            breakpoint: 992,
+            settings: {
+          
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+         
+            }
+          },
+          {
+            breakpoint: 576,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              dots: false
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+      
+        });
+         //about patner slider END;
+
 
 });      
